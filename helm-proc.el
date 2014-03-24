6d6df58de1059538e8781b9671915fd7d0edc6b0
Markus Hauck <markus1189@gmail.com>
2014-03-24 19:54:49 +0100

    Add emasc23 dependency

--- helm-proc.el
+++ helm-proc.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Markus Hauck <markus1189@gmail.com>
 ;; Keywords: helm
 ;; Version: 0.0.1
-;; Package-requires: ((helm "1.6.0"))
+;; Package-requires: ((emacs "23") (helm "1.6.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
